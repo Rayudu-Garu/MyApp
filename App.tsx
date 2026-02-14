22864e03b89d3eaf52febe3f3321560215a1b416
Subbarayudu.Nidumolu <subbarayudu.nidumolu@SubbarayuduNidumolus-MacBook-Air.local>
2026-02-14 23:07:08 +0530

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,44 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React,{useState,useRef} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,  } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [count,setCount]=useState(0);
+  const [timer,setTimer] = useState(false);
+  const ref =useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const startTimer=()=>{
+    if(ref.current !== null) return;
+    setTimer(true);
+    ref.current = setTimeout(()=>{
+      setCount(count => count+1);
+    },1000);
+    
+  }
+
+  const stopTimer =()=>{
+    if(ref.current === null) return;
+    clearTimeout(ref.current);
+    ref.current = null;
+    setTimer(false);
+  }
+
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+    <SafeAreaProvider style={{backgroundColor:'black'}}> 
+      <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.text}>{count}</Text>
+        <TouchableOpacity onPress={startTimer}>
+          <View style={{backgroundColor:'grey',borderRadius:10,padding:10,marginBottom:10}}>
+          <Text style={styles.text}>Start Timer</Text>
+          </View>
+        </TouchableOpacity>
+         <TouchableOpacity onPress={stopTimer}>
+          <View style={{backgroundColor:'grey',borderRadius:10,padding:10,marginBottom:10}}>
+          <Text style={styles.text}>Stop Timer</Text>
+          </View>
+        </TouchableOpacity>
+         <TouchableOpacity onPress={()=>{setCount(0); stopTimer();}}>
+          <View style={{backgroundColor:'grey',borderRadius:10,padding:10}}>
+          <Text style={styles.text}>Reset</Text>
+          </View>
+        </TouchableOpacity>
+
+        </View>
+      </SafeAreaView>
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ebf5ecff',
+    height:'100%',
+    top:40
+  },
+  text: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#333',
+    width:'100%',
+    textAlign:'center'
   },
 });
 
